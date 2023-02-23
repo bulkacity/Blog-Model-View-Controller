b869d43a37abe3facff19d65a74053fb8df1c72d
bulkacity <112503952+bulkacity@users.noreply.github.com>
2023-02-23 09:02:08 -0500

    added all the remaining code

--- public/js/blogEdit.js
+++ public/js/blogEdit.js
@@ -1,37 +1,68 @@
-const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      try {
-        const response = await fetch(`/api/blogPosts/${id}`, {
-          method: 'DELETE',
-        });
-  
-        if (response.ok) {
-          document.location.reload();
-        } else {
-          alert('Failed to delete blogPost');
-        }
-      } catch (err) {
-        console.error(err);
-        alert('Failed to delete blogPost');
-      }
-    }
-  };
-  
-  const editButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-      document.location.replace(`/edit/${id}`);
-    }
-  };
-  
-  document.querySelector('.blogPost-list')
-    .addEventListener('click', async (event) => {
-      if (event.target.classList.contains('delete-btn')) {
-        await delButtonHandler(event);
-      } else if (event.target.classList.contains('edit-btn')) {
-        await editButtonHandler(event);
-      }
+// Get a reference to the edit button and the textarea
+const editBtn = document.getElementById('edit-btn');
+const blogPostDesc = document.getElementById('blogPost-desc');
+ if (editBtn) {
+  console.log("found edit button");
+ }
+// Add a click event listener to the edit button
+editBtn.addEventListener('click', () => {
+  // Display the textarea
+  console.log('Edit button clicked');
+  blogPostDesc.style.display = 'block';
+});
+
+const editBtns = document.querySelectorAll('.edit-btn');
+const blogPostContent = document.querySelector('.blogPost-content');
+
+// Add a click event listener to each edit button
+editBtns.forEach(editBtn => {
+  editBtn.addEventListener('click', () => {
+    // Create a text box and submit button
+    const textBox = document.createElement('textarea');
+    const submitBtn = document.createElement('button');
+
+    // Set the attributes of the text box and submit button
+    textBox.setAttribute('class', 'form-input');
+    textBox.setAttribute('id', 'blogPost-desc');
+    textBox.setAttribute('name', 'blogPost-desc');
+
+    submitBtn.setAttribute('class', 'btn btn-sm btn-primary submit-btn');
+    submitBtn.setAttribute('data-id', editBtn.getAttribute('data-id'));
+    submitBtn.textContent = 'Submit';
+
+    // Insert the text box and submit button into the blog post content
+    blogPostContent.appendChild(textBox);
+    blogPostContent.appendChild(submitBtn);
+
+    // Remove the edit button
+    editBtn.style.display = 'none';
+
+    // Add a click event listener to the submit button
+    submitBtn.addEventListener('click', () => {
+      // Get the value of the text box
+      const blogPostDesc = textBox.value;
+
+      // Make a PUT request to update the blog post description
+      fetch(`/blogPosts/${submitBtn.getAttribute('data-id')}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ description: blogPostDesc })
+      })
+      .then(response => response.json())
+      .then(data => {
+        // Update the blog post description in the UI
+        const blogPostDescElem = document.querySelector(`[data-id="${data.id}"] .blogPost-desc`);
+        blogPostDescElem.textContent = data.description;
+
+        // Remove the text box and submit button
+        blogPostContent.removeChild(textBox);
+        blogPostContent.removeChild(submitBtn);
+
+        // Show the edit button again
+        editBtn.style.display = 'block';
+      })
+      .catch(error => console.error(error));
     });
-  
+  });
+});
+
--- public/js/profile.js
+++ public/js/profile.js
@@ -37,6 +37,59 @@ const delButtonHandler = async (event) => {
   }
 };
 
+const editBtns = document.querySelectorAll('.edit-btn');
+
+editBtns.forEach(editBtn => {
+  editBtn.addEventListener('click', () => {
+    const blogPostContent = editBtn.parentNode.parentNode; // get the blog post content container
+    const blogPostDesc = blogPostContent.querySelector('.blogPost-desc'); // get the blog post description
+    const blogPostDescText = blogPostDesc.textContent; // get the text content of the description
+
+    // create the input text box and set its value to the current description
+    const textBox = document.createElement('input');
+    textBox.setAttribute('type', 'text');
+    textBox.setAttribute('class', 'form-input');
+    textBox.setAttribute('id', 'blogPost-desc');
+    textBox.setAttribute('name', 'blogPost-desc');
+    textBox.setAttribute('value', blogPostDescText);
+
+    // replace the description paragraph with the input text box
+    blogPostContent.replaceChild(textBox, blogPostDesc);
+
+    // change the edit button to a save button
+    editBtn.textContent = 'SAVE';
+    editBtn.classList.remove('btn-primary');
+    editBtn.classList.add('btn-success');
+
+    // add a click event listener to the save button to update the blog post description
+    editBtn.addEventListener('click', () => {
+      const newDescText = textBox.value; // get the new description text
+      const blogPostId = blogPostContent.getAttribute('data-id'); // get the blog post ID
+
+      // make a PUT request to update the blog post description
+      fetch(`/api/blogPosts/${blogPostId}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ description: newDescText })
+      })
+      .then(response => response.json())
+      .then(data => {
+        // replace the input text box with a new description paragraph
+        const newDesc = document.createElement('p');
+        newDesc.setAttribute('class', 'blogPost-desc');
+        newDesc.textContent = data.description;
+        blogPostContent.replaceChild(newDesc, textBox);
+
+        // change the save button back to an edit button
+        editBtn.textContent = 'EDIT';
+        editBtn.classList.remove('btn-success');
+        editBtn.classList.add('btn-primary');
+      })
+      .catch(error => console.error(error));
+    });
+  });
+});
+
 const editFormHandler = async (event) => {
   event.preventDefault();
 
